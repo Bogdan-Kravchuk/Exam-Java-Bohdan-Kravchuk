ff7048f8c2114e2e6a32a012f4e4e28d8035141f
Bohdan Kravchuk <bogdankravchuk.game@gmail.com>
2026-04-28 09:59:28 +0300

    feat: add JUNIT tests more

--- src/test/java/CreatePetTest.java
+++ src/test/java/CreatePetTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvSources;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,5 +19,21 @@ public class CreatePetTest {
         // assertThrows(new IllegalArgumentException(), ShelterZone.createCat("MyCat", 1200));
     }
 
-    
+
+   /* @ParameterizedTest
+    @CsvSources (value = {
+        //    "name", 5
+
+
+    })
+   // void createManyVolomteeres(){
+
+//Volunteer v = new Volunteer();
+
+    }
+
+
+
+
+    */
 }
